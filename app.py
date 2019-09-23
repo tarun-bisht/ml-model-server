d3ea1ae34bc0c2d48aaeb515612be664d2173de5
tarun-bisht <tarunbishttarun.11@gmail.com>
2019-09-23 16:38:12 +0530

    Dog Breed Classifier and Index File Added

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask_cors import CORS
 from flask import Flask,request,render_template,json,jsonify,send_from_directory
-import io
+import os
 app=Flask(__name__)
 global folder
 CORS(app)
+
+@app.route("/")
+def index():
+    files=os.listdir()
+    links=[]
+    for f in files:
+        if not os.path.isfile(f) and f.startswith('model_'):
+            links.append("/"+f)
+    return render_template('index.html',links=links)
+
 @app.route("/<model>")
 def model(model):
     data=json.load(open(model+"/model.json"))
